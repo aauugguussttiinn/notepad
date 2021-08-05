6fa4e1dfd509c249cfe52d7cd5bd220195171310
Augustin <augustin.berne.pro@gmail.com>
2021-08-05 14:12:35 +0200

    HTML converted works

--- src/components/NoteDisplay/index.jsx
+++ src/components/NoteDisplay/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import Showdown from 'showdown';
 
 const NoteDisplay = ({title, note}) => {
+  
+  const converter = new Showdown.Converter();
+  const convertedTitle = converter.makeHtml(title)
+  const convertedContent = converter.makeHtml(note)
+
+  function createMarkup(data) {
+    return {__html: data};
+  }
+
   return (
     <>
-      <div>{title}</div>
-      <div>{note}</div>
+      <div>Your title : {title}</div>
+      <div>Your note : {note}</div>
+      <div dangerouslySetInnerHTML={createMarkup(convertedTitle)} />
+      <div dangerouslySetInnerHTML={createMarkup(convertedContent)} />
     </>
   );
 };
