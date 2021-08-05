5c6104148ab438f12adc3065553ea32161eab9a7
Augustin <augustin.berne.pro@gmail.com>
2021-08-05 17:38:03 +0200

    Rendering localstorage

---
+++ src/components/NoteList/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NoteList = () => {
+  const [localStorage, setLocalStorage] = React.useState('');
+  
+  
+  return (
+    <div>test</div>
+  )
+}
+
+export default NoteList;
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./main.css";
+import NoteList from 'components/NoteList';
 import MarkdownInput from 'components/MarkdownInput';
 
 
 const App = () => (
   <main className="d-flex">
     <div className="col-4 border column-full-height">
-      Test
+      <NoteList />
     </div>
     <div className="col-8 column-full-height">
       <MarkdownInput />
